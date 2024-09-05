cb4b31127a9a4ac02aedab322c6b69b5b2b14cd4
GSL006 <111833811+GSL006@users.noreply.github.com>
2024-09-05 16:52:11 +0530

    Update Approach.tsx

--- components/Approach.tsx
+++ components/Approach.tsx
@@ -50,7 +50,7 @@ const Approach = () => {
           title="Development & Launch"
           icon={<AceternityIcon order="Phase 3" />}
           des="This is where the magic happens! Based on the approved design, 
-          I'll translate everything into functional code, building the website
+          I'll translate everything into functional code, building the app
           from the ground up."
         >
           <CanvasRevealEffect
