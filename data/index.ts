40bba6aece25463baee3d971745eb726e6438436
GSL006 <gagansl62004@gmail.com>
2024-11-12 17:58:30 +0530

    Nov 12

--- data/index.ts
+++ data/index.ts
@@ -49,7 +49,7 @@ export const gridItems = [
 
   {
     id: 5,
-    title: "Currently building a model for Predictive Maintenance for EV's",
+    title: "Open to freelance in Full Stack Development, Machine Learning, and Data Analytics.",
     description: "The Inside Scoop",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
