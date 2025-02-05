a6484ac9914c7ff7258910bc8a5b261a428c7f1e
GSL006 <gagansl62004@gmail.com>
2025-02-05 16:55:05 +0530

    Feb 5

--- data/index.ts
+++ data/index.ts
@@ -49,7 +49,7 @@ export const gridItems = [
 
   {
     id: 5,
-    title: "Working on Trading bots for Decentralized trading platforms",
+    title: "Upcoming C++ Developer(Intern) at GoQuant",
     description: "The Inside Scoop",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
