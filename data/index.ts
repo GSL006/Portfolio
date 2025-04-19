5319a837eb83b44e0fc1d3a77a4b761493e53e2b
GSL006 <gagansl62004@gmail.com>
2025-04-20 00:35:35 +0530

    Scoop v3

--- data/index.ts
+++ data/index.ts
@@ -49,7 +49,7 @@ export const gridItems = [
 
   {
     id: 5,
-    title: "Working on NeuroMimic A Digital Brain Clone",
+    title: "Working on NeuroMimic  A Digital Brain Clone",
     description: "The Inside Scoop",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
