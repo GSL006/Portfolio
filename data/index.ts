4650e56e369e31d0a7b6d0af478cad26e54c6301
GSL006 <gagansl62004@gmail.com>
2025-04-20 00:20:34 +0530

    Inside Scoop updated

--- data/index.ts
+++ data/index.ts
@@ -49,7 +49,7 @@ export const gridItems = [
 
   {
     id: 5,
-    title: "Working on a trasformer based method for stock price and trend prediction instead of traditional ML models.",
+    title: "Working on NeuroMimic: A Digital Brain Clone",
     description: "The Inside Scoop",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
